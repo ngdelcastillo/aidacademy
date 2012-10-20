2af862ee5135f6ac25719884f284d1030bd3ce50
Noel del Castillo <ngdelcastillo@gmail.com>
2012-10-21 01:21:53 +0800

    modified email config to fix the confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Aidacademy::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -32,16 +29,16 @@ Aidacademy::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:5000' }
   config.action_mailer.delivery_method = :smtp
   # change to false to prevent email from being sent during development
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "pragtechnology.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "dev",
+    user_name: "devaidacademy",
     password: "devmachine"
   }
   
