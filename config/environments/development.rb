58541073cfd7d28ca55eb5ff73c235faafa0e343
Noel del Castillo <ngdelcastillo@gmail.com>
2012-10-20 22:19:06 +0800

    added devise, remove index, added mail config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,21 @@ Aidacademy::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:5000' }
+  config.action_mailer.delivery_method = :smtp
+  # change to false to prevent email from being sent during development
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "pragtechnology.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "dev",
+    password: "devmachine"
+  }
 end
